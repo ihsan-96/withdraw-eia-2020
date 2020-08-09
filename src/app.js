1d5eb272c7199fe58fa9976a6c0b339e2111ea3b
ihsan-96 <ameenahsanma@gmail.com>
2020-08-10 00:26:00 +0530

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,13 +1,43 @@
 
 const express = require('express')
+const storage = require('node-persist')
+
+let COUNTER = 0;
+
 const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
+  COUNTER++
   res.sendFile(__dirname + '/index.html')
+  storage.setItem('counter', COUNTER)
+  .then(()=>0).catch(e=>console.log('set_error', e))
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('/count', (req, res) => {
+  console.log('hello from server')
+  res.json({count: COUNTER + 573});
 })
+
+storage.init()
+.then(() => {
+  storage.getItem('counter')
+  .then(count => {
+    COUNTER = count > 0 ? count : 0
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`)
+    })
+  }).catch(e=>{
+    console.log('get_error', e)
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`)
+    })
+  })
+}).catch(e=>{
+  console.log('init_err', e)
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+})
+
 
